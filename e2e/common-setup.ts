c498856a30c191b1d0077f8c99dccb1ae9942697
Tom Weatherhead <tomw@2hrd4u.org>
2020-11-07 12:55:36 -0700

    Dev

--- e2e/common-setup.ts
+++ e2e/common-setup.ts
@@ -1,79 +1,83 @@
 // angular-electron/e2e/common-setup.ts
 
-import { platform } from 'os';
+// import { platform } from 'os';
 
 import { join } from 'path';
 
 import { Application } from 'spectron';
 
 // const electronPath = require('electron'); // Require Electron from the binaries included in node_modules.
-// import * as electron from 'electron';
+import * as electron from 'electron';
 
-function getElectronPath(): string {
-	let str: string;
+// function getElectronPath(): string {
+// 	let str: string;
 
-	switch (platform()) {
-		case 'darwin':
-			str = 'Electron.app/Contents/MacOS/Electron';
-			break;
+// 	switch (platform()) {
+// 		case 'darwin':
+// 			str = 'Electron.app/Contents/MacOS/Electron';
+// 			break;
 
-		case 'linux':
-			str = 'electron';
-			break;
+// 		case 'linux':
+// 			str = 'electron';
+// 			break;
 
-		// case 'win32':
-		// 	str = '';
-		// 	break;
+// 		// case 'win32':
+// 		// 	str = '';
+// 		// 	break;
 
-		default:
-			throw new Error(
-				`e2e: getElectronPath() : Unexpected platform '${platform()}'`
-			);
-	}
+// 		default:
+// 			throw new Error(
+// 				`e2e: getElectronPath() : Unexpected platform '${platform()}'`
+// 			);
+// 	}
 
-	return join(__dirname, '../node_modules/electron/dist', str);
-}
+// 	return join(__dirname, '../node_modules/electron/dist', str);
+// }
 
 export default function setup(): void {
 	beforeEach(async function () {
 		console.log('BEGIN common-setup beforeEach');
-		if (platform() === 'linux') {
-			this.app = new Application({
-				// path: join(
-				// 	__dirname,
-				// 	'../release/angular-electron-0.0.3.AppImage'
-				// ),
-				path: '/home/travis/build/tom-weatherhead/angular-electron/release/angular-electron-0.0.3.AppImage',
-				// args: [join(__dirname, '..')],
-				args: ['/home/travis/build/tom-weatherhead/angular-electron'],
-				webdriverOptions: {}
-			});
-		} else {
-			this.app = new Application({
-				// Your electron path can be any binary
-				// i.e for OSX an example path could be '/Applications/MyApp.app/Contents/MacOS/MyApp'
-				// But for the sake of the example we fetch it from our node_modules.
-				path: getElectronPath(),
+		// if (platform() === 'linux') {
+		// 	this.app = new Application({
+		// 		// path: join(
+		// 		// 	__dirname,
+		// 		// 	'../release/angular-electron-0.0.3.AppImage'
+		// 		// ),
+		// 		path:
+		// 			'/home/travis/build/tom-weatherhead/angular-electron/release/angular-electron-0.0.3.AppImage',
+		// 		args: [join(__dirname, '..')],
+		// 		// args: ['/home/travis/build/tom-weatherhead/angular-electron'],
+		// 		webdriverOptions: {}
+		// 	});
+		// } else {
+		const electronPath = electron.toString();
+		console.log('electronPath is', typeof electronPath, electronPath);
+		this.app = new Application({
+			// Your electron path can be any binary
+			// i.e for OSX an example path could be '/Applications/MyApp.app/Contents/MacOS/MyApp'
+			// But for the sake of the example we fetch it from our node_modules.
+			// path: getElectronPath(),
+			path: electronPath,
 
-				// Assuming you have the following directory structure
+			// Assuming you have the following directory structure
 
-				//  |__ my project
-				//     |__ ...
-				//     |__ main.js
-				//     |__ package.json
-				//     |__ index.html
-				//     |__ ...
-				//     |__ test
-				//        |__ spec.js  <- You are here! ~ Well you should be.
+			//  |__ my project
+			//     |__ ...
+			//     |__ main.js
+			//     |__ package.json
+			//     |__ index.html
+			//     |__ ...
+			//     |__ test
+			//        |__ spec.js  <- You are here! ~ Well you should be.
 
-				// The following line tells spectron to look and use the main.js file
-				// and the package.json located 1 level above.
-				args: [join(__dirname, '..')],
-				webdriverOptions: {}
-			});
-		}
+			// The following line tells spectron to look and use the main.js file
+			// and the package.json located 1 level above.
+			args: [join(__dirname, '..')],
+			webdriverOptions: {}
+		});
+		// }
 
-		console.log('this.app is', typeof this.app, this.app);
+		// console.log('this.app is', typeof this.app, this.app);
 
 		console.log('Starting the app...');
 
@@ -86,13 +90,13 @@ export default function setup(): void {
 
 		console.log('The app has started.');
 
-		if (this.app) {
-			console.log(
-				'this.app.client is',
-				typeof this.app.client,
-				this.app.client
-			);
-		}
+		// if (this.app) {
+		// 	console.log(
+		// 		'this.app.client is',
+		// 		typeof this.app.client,
+		// 		this.app.client
+		// 	);
+		// }
 
 		console.log('END common-setup beforeEach');
 	});
--- e2e/main.e2e.ts
+++ e2e/main.e2e.ts
@@ -3,6 +3,8 @@
 import { expect } from 'chai';
 import { SpectronClient } from 'spectron';
 
+// import * as electron from 'electron';
+
 import commonSetup from './common-setup';
 
 describe('angular-electron App', () => {
@@ -26,6 +28,7 @@ describe('angular-electron App', () => {
 
 	it('Creates initial app window', async () => {
 		console.log('BEGIN test: Creates initial app window');
+		// console.log('electron is', typeof electron, electron);
 		const count = await client.getWindowCount();
 		// expect(count).toEqual(1);
 		expect(count).to.equal(1);
@@ -38,3 +41,54 @@ describe('angular-electron App', () => {
 	// 	expect(text).toEqual('App works !');
 	// });
 });
+
+// const Application = require('spectron').Application;
+// const assert = require('assert');
+// const electronPath = require('electron'); // Require Electron from the binaries included in node_modules.
+// const path = require('path');
+
+// import * as electron from 'electron';
+
+// describe('Application launch', function () {
+// 	this.timeout(10000);
+
+// 	beforeEach(function () {
+// 		this.app = new Application({
+// 			// Your electron path can be any binary
+// 			// i.e for OSX an example path could be '/Applications/MyApp.app/Contents/MacOS/MyApp'
+// 			// But for the sake of the example we fetch it from our node_modules.
+// 			path: electronPath,
+
+// 			// Assuming you have the following directory structure
+
+// 			//  |__ my project
+// 			//     |__ ...
+// 			//     |__ main.js
+// 			//     |__ package.json
+// 			//     |__ index.html
+// 			//     |__ ...
+// 			//     |__ test
+// 			//        |__ spec.js  <- You are here! ~ Well you should be.
+
+// 			// The following line tells spectron to look and use the main.js file
+// 			// and the package.json located 1 level above.
+// 			args: [path.join(__dirname, '..')]
+// 		});
+
+// 		return this.app.start();
+// 	});
+
+// 	afterEach(function () {
+// 		if (this.app && this.app.isRunning()) {
+// 			return this.app.stop();
+// 		}
+// 	});
+
+// 	it('shows an initial window', function () {
+// 		return this.app.client.getWindowCount().then(function (count) {
+// 			assert.equal(count, 1);
+// 			// Please note that getWindowCount() will return 2 if `dev tools` are opened.
+// 			// assert.equal(count, 2)
+// 		});
+// 	});
+// });
